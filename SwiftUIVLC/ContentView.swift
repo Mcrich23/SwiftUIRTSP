446f41e856f50b608df3ffaf85926d3c55712055
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-09-27 17:39:52 -0700

    Initial Commit

---
+++ SwiftUIVLC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIVLC
+//
+//  Created by Morris Richman on 9/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIVLC/SwiftUIVLCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVLCApp.swift
+//  SwiftUIVLC
+//
+//  Created by Morris Richman on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVLCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
