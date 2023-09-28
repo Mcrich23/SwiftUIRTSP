2fe6352d77406248eb6e89e9e0c6395ad7627aba
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-09-27 19:09:06 -0700

    Added RTSP Support

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftUIVLC' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftUIVLC
+pod 'MobileVLCKit', '~>3.3.0'
+
+end
--- SwiftUIVLC/ContentView.swift
+++ SwiftUIVLC/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+import AVFoundation
+import AVKit
+
+struct RTSPStreamView: View {
+    // RTSP Stream URL
+    private let rtspURL: URL // Replace with your RTSP stream URL
+
+    // Initialize AVPlayer and AVPlayerLayer
+    @State private var player: AVPlayer?
+    @State private var playerLayer: AVPlayerLayer?
+    
+    // Initialize PiP view controller
+    @State private var pipViewController: AVPictureInPictureController?
+    
+    init(rtspURL: URL) {
+        self.rtspURL = rtspURL
+        setupPlayer()
+    }
+
+    private func setupPlayer() {
+        // Create AVPlayer and set its player item to the RTSP stream
+        let playerItem = AVPlayerItem(url: rtspURL)
+        player = AVPlayer(playerItem: playerItem)
+
+        // Create AVPlayerLayer for video rendering
+        let layer = AVPlayerLayer(player: player)
+        
+        playerLayer = layer
+        playerLayer?.videoGravity = .resizeAspect
+
+        // Initialize PiP controller
+        pipViewController = AVPictureInPictureController(playerLayer: layer)
+        
+        // Start streaming
+        player?.play()
+    }
+
+    var body: some View {
+        VideoPlayer(player: player) {
+            Text("Video playback failed.")
+        }
+        Button("Enter PiP") {
+            pipViewController?.startPictureInPicture()
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        RTSPStreamView(rtspURL: URL(string: "YOUR_RTSP_STREAM_URL_HERE")!)
+            .frame(width: 320, height: 240) // Set your desired frame size
     }
 }
 
